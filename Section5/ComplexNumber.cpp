3bd63e55308756f5ceb26893f36aeb6c84b92937
Resolt <philip@neuronsinc.com>
2018-10-17 14:18:05 +0200

    -more stuff

--- Section5/ComplexNumber.cpp
+++ Section5/ComplexNumber.cpp
@@ -8,10 +8,25 @@ int main()
 	com1 = com2;
 	std::cout << com1 << com2 << Complex(3, 2) << std::endl;
 
-	Complex com4 = com1 + com2;
-	com4 = com4 + com3;
+	Complex com4 = com1 + com2 + com3;
+	// com4 = com4 + com3;
 
 	std::cout << com4 << std::endl;
+
+	Complex com5 = 15 + com1 + 2;
+	std::cout << com5 << std::endl;
+
+	// if (com1 == com2)
+	// {
+	// 	std::cout << "YES" << std::endl;
+	// }
+
+	if (com1 != Complex(0, 5))
+	{
+		std::cout << "YES" << std::endl;
+	}
+
+	std::cout << com1 << !com1 << std::endl;
 
 	return 0;
 }
--- Section5/ComplexNumber.hpp
+++ Section5/ComplexNumber.hpp
@@ -6,40 +6,70 @@ private:
 	double real, imaginary;
 
 public:
-	Complex(): real(0), imaginary(0) {};
-	Complex(const double &real, const double &imaginary) : real(real), imaginary(imaginary) {};
+	Complex(): real(0), imaginary(0)
+	{
+		std::cout << "Default" << std::endl;
+	};
+	Complex(const double &real, const double &imaginary) : real(real), imaginary(imaginary)
+	{
+		std::cout << "Overload" << std::endl;
+	};
 	Complex(const Complex &other) // COPY CONSTRUCTOR
 	{
+		std::cout << "Copy" << std::endl;
 		this->real = other.real;
 		this->imaginary = other.imaginary;
-		std::cout << "Complex copy" << std::endl;
 	};
-	const Complex &operator = (const Complex &other) // OVERLOADED ASSIGNMENT OPERATOR
+	const Complex &operator = (const Complex &other)
 	{
+		std::cout << "=" << std::endl;
 		this->real = other.real;
 		this->imaginary = other.imaginary;
-
-		std::cout << "Ass. operator" << std::endl;
-
 		return *this;
 	}
 
-	const Complex &operator + (const Complex &other) // OVERLOADED ASSIGNMENT OPERATOR
+	friend Complex operator + (const Complex &c1, const Complex &c2)
 	{
-		this->real += other.real;
-		this->imaginary += other.imaginary;
-
-		std::cout << "Plus. operator" << std::endl;
-
-		return *this;
+		std::cout << "+" << std::endl;
+		return Complex(c1.getReal() + c2.getReal(), c1.getImaginary() + c2.getImaginary());
 	}
 
-	friend std::ostream &operator<<(std::ostream &out, const Complex &other)
+	friend Complex operator + (const double &d, const Complex &c)
 	{
+		std::cout << "+dl" << std::endl;
+		return Complex(c.getReal() + d, c.getImaginary());
+	}
+
+	friend Complex operator + (const Complex &c, const double &d)
+	{
+		std::cout << "+dr" << std::endl;
+		return Complex(c.getReal() + d, c.getImaginary());
+	}
+
+	friend std::ostream &operator << (std::ostream &out, const Complex &other)
+	{
+		std::cout << "Out" << std::endl;
 		out << "(" << other.getReal() << ", " << other.getImaginary() << ")";
-
 		return out;
 	};
+
+	friend bool operator == (const Complex &c1, const Complex &c2)
+	{
+		std::cout << "==" << std::endl;
+		return c1.getReal() == c2.getReal() && c1.getImaginary() == c2.getImaginary();
+	}
+
+	friend bool operator != (const Complex &c1, const Complex &c2)
+	{
+		std::cout << "!=" << std::endl;
+		return !(c1.getReal() == c2.getReal() && c1.getImaginary() == c2.getImaginary());
+	}
+
+	Complex operator ! ()
+	{
+		std::cout << "!" << std::endl;
+		return Complex(real, -imaginary);
+	}
 
 	double getReal() const { return this->real; };
 	double getImaginary() const { return this->imaginary; };
