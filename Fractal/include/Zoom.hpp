0fdac456a04a56e04bad796f2ee73947f9d9b7bd
Resolt <pdmmichaelsen@gmail.com>
2018-10-28 16:52:45 +0100

    -more stuff

--- Fractal/include/Zoom.hpp
+++ Fractal/include/Zoom.hpp
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <vector>
+#include <iostream>
 
 namespace fract
 {
@@ -12,18 +13,25 @@ struct Zoom
 	uint16_t x{0}, y{0};
 	double scale{0.0};
 
-	Zoom(uint16_t x, uint16_t y, double scale) : x(x), y(y), scale(scale){};
+	// Zoom(uint16_t x, uint16_t y, double scale) : x(x), y(y), scale(scale){};
+	// virtual ~Zoom()
+	// {
+	// 	std::cout << "No segfault" << std::endl;
+	// };
 };
 
 class ZoomList
 {
 private:
 	uint16_t m_width{0}, m_height{0};
+	double m_xCenter{0.0}, m_yCenter{0.0}, m_scale{1.0};
 	std::vector<Zoom> zooms;
 
 public:
-	ZoomList(uint16_t width, uint16_t height);
-	void add(const Zoom &zoom);
+	ZoomList(uint16_t width, uint16_t height) : m_width(width), m_height(height){};
+	virtual ~ZoomList() {};
+	void add(uint16_t x, uint16_t y, double scale);
+	Zoom getZoom(uint16_t x, uint16_t y, double scale);
 	std::pair<double, double> doZoom(uint16_t x, uint16_t y);
 };
 
--- Fractal/src/Zoom.cpp
+++ Fractal/src/Zoom.cpp
@@ -3,14 +3,26 @@
 namespace fract
 {
 
-ZoomList::ZoomList(uint16_t width, uint16_t height) : m_width(width), m_height(height)
+void ZoomList::add(uint16_t x, uint16_t y, double scale)
 {
+	
+	this->zooms.push_back(getZoom(x, y, scale));
 
+	this->m_xCenter += (x - (double)this->m_width / 2) * this->m_scale;
+	this->m_yCenter += (y - (double)this->m_height / 2) * this->m_scale;
+
+	this->m_scale = this->m_scale * scale;
+
+	std::cout << this->m_xCenter << " " << this->m_yCenter << " " << this->m_scale << std::endl;
 }
 
-void ZoomList::add(const Zoom &zoom)
+Zoom ZoomList::getZoom(uint16_t x, uint16_t y, double scale)
 {
-	zooms.push_back(zoom);
+	Zoom z;
+	z.x = x;
+	z.y = y;
+	z.scale = scale;
+	return z;
 }
 
 std::pair<double, double> ZoomList::doZoom(uint16_t x, uint16_t y)
